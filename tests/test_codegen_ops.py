76a865ffcdc742d4455274b1b35d674928342c0c
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-06 04:06:16 +0100

    Add mm to codegen op coverage

--- tests/test_codegen_ops.py
+++ tests/test_codegen_ops.py
@@ -585,6 +585,7 @@ CODEGEN_ATEN_OPS = [
     torch.ops.aten.addbmm.default,
     torch.ops.aten.addmv.default,
     torch.ops.aten.addr.default,
+    torch.ops.aten.mm.default,
     torch.ops.aten.matmul.default,
     torch.ops.aten.max_pool1d.default,
     torch.ops.aten.max_pool2d.default,
@@ -942,6 +943,10 @@ CODEGEN_OP_TEST_CONFIG = {
         "allowed_dtypes": (torch.float32,),
     },
     torch.ops.aten.matmul.default: {
+        "allow_noncontiguous": True,
+        "requires_same_shape": False,
+    },
+    torch.ops.aten.mm.default: {
         "allow_noncontiguous": True,
         "requires_same_shape": False,
     },
