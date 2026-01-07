fd87c9b3125e2b096a59580ae5185ccf54ffa595
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-07 06:18:11 +0100

    Add onnx2c golden tests (#247)

---
+++ tests/onnx2c_refs/test_onnx04_identity_tiny.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "ops_scalar_f32.h"
+
+void node1_clone_f32(const float a[1][3], float out[1][3]) {
+    for (int64_t i0 = 0; i0 < 1; ++i0) {
+        for (int64_t i1 = 0; i1 < 3; ++i1) {
+            out[i0][i1] = a[i0][i1];
+        }
+    }
+}
+
+void ref_codegen_main_f32(const float input_0[1][3], float out[1][3]) {
+    node1_clone_f32(input_0, out);
+}
+
+void model_run(const float* in0, float* out0) {
+    ref_codegen_main_f32(in0, out0);
+}
---
+++ tests/onnx2c_refs/test_onnx06_matmul_tiny.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "ops_scalar_f32.h"
+
+static const float weight_initializers_onnx_initializer_0[4] = {
+    1.0f, 0.0f, 0.0f, 1.0f
+};
+
+void node1_matmul_f32(const float a[1][2], const float b[2][2], float out[1][2]) {
+        for (int64_t i = 0; i < 1; ++i) {
+            for (int64_t j = 0; j < 2; ++j) {
+                float acc = 0.0f;
+                for (int64_t t = 0; t < 2; ++t) {
+                    acc += a[i][t] * b[t][j];
+                }
+                out[i][j] = acc;
+            }
+        }
+}
+
+void ref_codegen_main_f32(const float input_0[1][2], const float input_1[2][2], float out[1][2]) {
+    node1_matmul_f32(input_0, input_1, out);
+}
+
+void model_run(const float* in0, float* out0) {
+    ref_codegen_main_f32(in0, weight_initializers_onnx_initializer_0, out0);
+}
