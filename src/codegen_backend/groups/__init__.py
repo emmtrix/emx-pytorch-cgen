0bbf4fac5a7309da840db33989e0d081850648bd
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-07 23:56:04 +0100

    Add group registration API

--- src/codegen_backend/groups/__init__.py
+++ src/codegen_backend/groups/__init__.py
@@ -1,3 +1,7 @@
-from codegen_backend.groups.registry import GroupRegistry, get_group_registry
+from codegen_backend.groups.registry import (
+    GroupRegistry,
+    get_group_registry,
+    register_group,
+)
 
-__all__ = ["GroupRegistry", "get_group_registry"]
+__all__ = ["GroupRegistry", "get_group_registry", "register_group"]
