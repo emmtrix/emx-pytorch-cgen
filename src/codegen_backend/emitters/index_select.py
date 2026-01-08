4f36b3ce2258ef406efc70f644b7c1a8eeac3285
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-08 20:50:23 +0100

    Refactor index_select template emission (#379)

--- src/codegen_backend/emitters/index_select.py
+++ src/codegen_backend/emitters/index_select.py
@@ -10,8 +10,6 @@ from codegen_backend.emitters.base import (
     KindEmitterBase,
     _format_array_suffix,
     _is_contiguous,
-    emit_footer,
-    emit_loops,
 )
 from codegen_backend.errors import CodegenBackendError
 from codegen_backend.indexing import _emit_strided_access
@@ -46,7 +44,6 @@ def _write_index_select_kernel(
         f"const {index_c_type} index{index_suffix}, "
         f"{dtype.c_type} out{out_suffix}) {{"
     )
-    loop_lines, indent = emit_loops(output_shape)
     output_indices = [f"i{dim}" for dim in range(len(output_shape))]
     output_access = _emit_strided_access(
         "out",
@@ -76,16 +73,13 @@ def _write_index_select_kernel(
         sizes=input_shape,
         c_type=dtype.c_type,
     )
-    body_lines = [
-        f"{indent}ssize_t idx = (ssize_t)({index_access});",
-        f"{indent}{output_access} = {input_access};",
-    ]
-    footer_lines = emit_footer(output_shape, indent)
     rendered = index_select_template.render(
         signature=signature,
-        loop_lines=loop_lines,
-        body_lines=body_lines,
-        footer_lines=footer_lines,
+        select_dim=select_dim,
+        output_shape=output_shape,
+        index_access=index_access,
+        input_access=input_access,
+        output_access=output_access,
     )
     return rendered.splitlines()
 
